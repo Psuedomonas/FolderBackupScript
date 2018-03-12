d8f6ce465ddf4e872606b7d6c3fe5945d20a3e6f
Pseudomonas <zehm0007@d.umn.edu>
2018-03-12 00:49:43 -0500

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,3 +1,3 @@
 
-xcopy c:\Dexis\* x:\backup\dexisBackup%date:~0,3%\* /Y /Q /S
-xcopy c:\Dentrix\* x:\backup\dentrixBackup%date:~0,3%\* /Y /Q /S
+xcopy c:\Dexis\* x:\backup\dexisBackup%date:~0,3%\* /Y /Q /S /Z
+xcopy c:\Dentrix\* x:\backup\dentrixBackup%date:~0,3%\* /Y /Q /S /Z
