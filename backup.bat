ece423d27a701d4b271e273c4de122aeaa152052
Pseudomonas <zehm0007@d.umn.edu>
2018-03-12 17:26:28 -0500

    Full date in filename (not time)

--- backup.bat
+++ backup.bat
@@ -1,2 +1,2 @@
-xcopy c:\Dexis x:\backup\dexisBackup%date:~0,3%\ /Y /S /Z
-xcopy c:\Dentrix x:\backup\dentrixBackup%date:~0,3%\ /Y /S /Z
+xcopy c:\Dexis x:\backup\dexisBackup%date:~-4,4%%date:~-7,2%\ /Y /S /Z
+xcopy c:\Dentrix x:\backup\dentrixBackup%date:~-4,4%%date:~-7,2%\ /Y /S /Z
